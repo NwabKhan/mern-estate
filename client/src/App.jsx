d1520de4056aa6c389b15f1d1f7b7ef1103bc21c
NwabKhan <m.junaid.89630@gmail.com>
2023-10-13 15:50:42 +0500

    profile page on header and private routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import PrivateRoutes from "./components/PrivateRoutes";
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element = { <Home />} />
-        <Route path="/about" element = { <About />} />
-        <Route path="/progile" element = { <Profile />} />
-        <Route path="/sign-in" element = { <SignIn />} />
-        <Route path="/sign-up" element = { <SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -9,7 +9,7 @@ import {
 import OAuth from "../components/OAuth";
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const { loading, error } = useSelector((state) => state.user);
+  const { loading, error } = useSelector((state) => state.user); //getting these states from redux
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
