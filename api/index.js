bb60e46727e7bfe458b0f1c86267ad8adddf17a6
NwabKhan <m.junaid.89630@gmail.com>
2023-10-19 16:10:06 +0500

    final touch, some command to help in deploy on render

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-
+import path from "path"; //To get dirtory name
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_KEY)
@@ -28,6 +28,14 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+const __dirname = path.resolve();
+// Creat a static folder and join it to /client/dist
+app.use(express.static(path.join(__dirname, "/client/dist"))); //if we use creat-reacy-app instead of vite it would be build file instead of dist
+
+//If we have any other address except the top 3(api/..)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // same as '/client/dist/index.html
+});
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
