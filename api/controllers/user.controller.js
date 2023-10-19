fd9cee8e4c9462fc2fa71b56520d3eaf196a16d1
NwabKhan <m.junaid.89630@gmail.com>
2023-10-19 11:40:07 +0500

    Search Route and backend functioanlity is added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import Listing from "../models/listing.model.js";
+import { query } from "express";
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
@@ -69,6 +70,61 @@ export const getUser = async (req, res, next) => {
     const { password: pass, ...rest } = user._doc;
 
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    //Offer, furnished, parking can be true, false or undefined, initially we want to take all those true or false
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      //if ther is search term take that else search for empty i.e all
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      //Search on the title:  regex used to seacrh in everywhere even inside the words etc .
+      //Also $options: "i" measn do not care about lower or uper case
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { deleteUser, updateUser, getUserListings, getUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  updateUser,
+  getUserListings,
+  getUser,
+  getListings
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 const router = express.Router();
 
 router.post("/update/:id", verifyToken, updateUser); //It first go to verifyToken then forwarded to updateUser
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
-router.get('/:id', verifyToken, getUser)
+router.get("/:id", verifyToken, getUser); //Get data to send message i.e Contact Landlord in Listing page
+router.get("/get", getListings); //Get all listing based on Search
 export default router;
